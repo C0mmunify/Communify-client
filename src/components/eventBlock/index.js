b5567d68a2ef08c42d7d87e71ed670437c18dd7c
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2023-02-15 09:41:12 +0000

    [TG] Adding event Block component placeholder

---
+++ src/components/eventBlock/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BackButton } from '../../components';
+
+const EventBlock = () => {
+
+    return(
+        <>
+        {/* <img src=></img> */}
+        {/* <h1> {EVENT TITLE} </h1> */}
+        {/* <h2> {EVENT LOCATION} </h2> */}
+        {/* <p> {EVENT DESCRIPTION} </p> */}
+        {/* <p> attendees: {EVENT ATTENDANCE} </p> */}
+        {/* <AttendButton /> */}
+        <BackButton />
+    </>
+    )
+}
+
+export default EventBlock
