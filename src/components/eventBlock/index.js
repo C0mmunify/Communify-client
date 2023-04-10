492577dd3e7a0bbfe17fac0f4a0941b7a3551634
Noah Platton <noahplatton@gmail.com>
2023-04-10 18:57:37 +0100

    [NP] Style event components

--- src/components/eventBlock/index.js
+++ src/components/eventBlock/index.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
 const EventBlock = (props) => {
+    let dateArray = props.date_occurring.split("T")[0].split("-");
+    let formattedDate = `${dateArray[2]}/${dateArray[1]}/${dateArray[0]}`;
 
-    let dateArray = props.date_occurring.split("T")[0].split("-")
-    let formattedDate = `${dateArray[2]}/${dateArray[1]}/${dateArray[0]}`
-    
-    return(
-        <section id='eventBlock'>
-            <section id='eventMain'>
-        { <h1> {props.title} </h1> }
-        { <h2> {props.location} </h2> }
-        { <p> {formattedDate} </p>}
+    return (
+        <section id="eventBlock">
+            <section id="eventMain">
+                {<h1> {props.title} </h1>}
+                {<h2> {props.location} </h2>}
+                {<p> {formattedDate} </p>}
+                <section id="eventDescription">
+                    {<p> {props.description} </p>}
+                </section>
+                <section id="eventAttendance">
+                    {
+                        <p>
+                            {" "}
+                            Attendees:{" "}
+                            {props.spaces_total - props.spaces_remaining}{" "}
+                        </p>
+                    }
+                    {<p> Spaces Remaining: {props.spaces_remaining} </p>}
+                </section>
+            </section>
         </section>
-        <section id='eventDescription'>
-        { <p> {props.description} </p> }
-        </section>
-        <section id='eventAttendance'>
-        { <p> Attendees: {props.spaces_total - props.spaces_remaining} </p> }
-        { <p> Spaces Remaining: {props.spaces_remaining} </p>}
-        </section>
-    </section>
-    )
-}
+    );
+};
 
-export default EventBlock
-
-
+export default EventBlock;
