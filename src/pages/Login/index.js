dd860e045df349d76c5d8d2fb0e75f5d766596e3
Noah Platton <noahplatton@gmail.com>
2023-04-11 18:04:54 +0100

    [NP] Formatting

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react';
-import { handleLogout } from '../../actions';
-import { BackButton, LoginForm } from '../../components';
-import './styles.css'
+import React, { useEffect } from "react";
+import { handleLogout } from "../../actions";
+import { BackButton, LoginForm } from "../../components";
+import "./styles.css";
 
 const Login = () => {
-
     useEffect(() => {
-        handleLogout()
-    }, [])
-    
-    return (
-    <section id='loginSection'>
-    <img src='https://drive.google.com/uc?export=view&id=1psAL2nh2-VuiS4XvU0sU259gGn3WDO6Y' alt='Communify Logo'></img>
-    <h1>Login</h1>
+        handleLogout();
+    }, []);
 
-    <LoginForm />
-    <BackButton />
-    </section>
-    )
-}
+    return (
+        <section id="loginSection">
+            <img
+                src="https://drive.google.com/uc?export=view&id=1psAL2nh2-VuiS4XvU0sU259gGn3WDO6Y"
+                alt="Communify Logo"
+            ></img>
+            <h1 id="login-title">Login</h1>
+
+            <LoginForm />
+            <BackButton />
+        </section>
+    );
+};
 
 export default Login;
